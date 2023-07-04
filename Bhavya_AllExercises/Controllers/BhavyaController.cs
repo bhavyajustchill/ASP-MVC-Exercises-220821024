be20110eb6ec145322db07ab2b9c7f55c58a26d0
Bhavya Popat <bhavyabpopat7777@gmail.com>
2023-07-04 07:58:33 +0530

    Initial Commit - 3 Exercises Done!

---
+++ Bhavya_AllExercises/Controllers/BhavyaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bhavya_AllExercises.Controllers
+{
+    public class BhavyaController : Controller
+    {
+        // GET: Bhavya
+        public ActionResult ExOne()
+        {
+            ViewBag.Name = "Bhavya Popat";
+            return View();
+        }
+
+        public ActionResult ExTwo()
+        {
+            ViewBag.BgColor = "#00cc99";
+            return View();
+        }
+
+        public ActionResult ExThree()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcessForm(string colorValue)
+        {
+            ViewBag.BgColor = colorValue;
+            return View("ExThree");
+        }
+    }
+}
---
+++ Bhavya_AllExercises/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bhavya_AllExercises.MvcApplication" Language="C#" %>
